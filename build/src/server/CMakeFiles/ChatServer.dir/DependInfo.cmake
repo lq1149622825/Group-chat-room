986f76e6f75e2df43b9f0bf2cc706e2202bbe192
lq1149622825 <lq13892023709@163.com>
2020-07-25 18:14:48 +0800

    项目代码提交

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lvqiang/Chat/src/server/chatserver.cpp" "/home/lvqiang/Chat/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/lvqiang/Chat/src/server/chatservice.cpp" "/home/lvqiang/Chat/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/lvqiang/Chat/src/server/db/db.cpp" "/home/lvqiang/Chat/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/home/lvqiang/Chat/src/server/main.cpp" "/home/lvqiang/Chat/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/lvqiang/Chat/src/server/model/friendmodel.cpp" "/home/lvqiang/Chat/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "/home/lvqiang/Chat/src/server/model/groupmodel.cpp" "/home/lvqiang/Chat/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/lvqiang/Chat/src/server/model/offlinemessage.cpp" "/home/lvqiang/Chat/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessage.cpp.o"
+  "/home/lvqiang/Chat/src/server/model/usermodel.cpp" "/home/lvqiang/Chat/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/lvqiang/Chat/src/server/redis/redis.cpp" "/home/lvqiang/Chat/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../thirdparty"
+  "../include/server/DB"
+  "../include/server/model"
+  "../include/server/redis"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
